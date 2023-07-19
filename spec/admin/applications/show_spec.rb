6306d62259992bba5a9d5c7907eb89e321909fea
Davis Weimer <davisweimer@gmail.com>
2023-07-19 17:50:58 -0600

    yippeeeeee model test away!

--- spec/admin/applications/show_spec.rb
+++ spec/admin/applications/show_spec.rb
@@ -54,7 +54,47 @@ RSpec.describe "admin applications/id page", type: :feature do
 
       expect(page).to_not have_button("Approve")
       expect(page).to_not have_button("Reject")
-      expect(page).to have_content("REJECTED!")
+      expect(page).to have_content("Rejected!")
+    end
+
+    it "displays button to reject/approve pet per application" do
+      visit "/adoption_apps/#{@adoption_app_1.id}"
+    
+      fill_in "Search", with: "Limb"
+      click_button "Submit"
+      click_button "Adopt this Pet"
+      
+      fill_in :good_owner_expl, with: "I eat cats and honestly I am a terrible person"
+      
+      click_button "Submit Application"
+      
+      @adoption_app_1.reload
+      
+
+      visit "/adoption_apps/#{@adoption_app_2.id}"
+    
+      fill_in "Search", with: "Limb"
+      click_button "Submit"
+      click_button "Adopt this Pet"
+      
+      fill_in :good_owner_expl, with: "I eat cats and honestly I am a terrible person"
+      
+      click_button "Submit Application"
+      
+      
+      
+      visit "/admin/applications/#{@adoption_app_1.id}"
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+      
+      click_button "Approve"
+
+      expect(page).to have_content("Approve")
+      
+      visit "/admin/applications/#{@adoption_app_2.id}"
+
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
     end
   end
 end
--- spec/models/adoption_app_spec.rb
+++ spec/models/adoption_app_spec.rb
@@ -1,8 +1,21 @@
 require "rails_helper"
 
 RSpec.describe AdoptionApp, type: :model do
+  before(:each) do
+    test_data
+  end
   describe "relationships" do
     it { should have_many(:adoption_app_pets) }
     it { should have_many(:pets), through: :adoption_app_pets }
   end
+
+  describe "instance methods" do
+    it "#shelter_finder" do
+
+      @adoption_app_1.pets.push(@pet_3)
+
+      expect(@adoption_app_1.shelter_finder).to eq(@pet_3.shelter.name)
+    end
+  end
+
 end
